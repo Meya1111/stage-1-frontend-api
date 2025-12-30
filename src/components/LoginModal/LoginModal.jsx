1e99852dd9d14cd3e3bbf18e31bfecb51d70b5bd
Meya Warrior <meyawarrior@gmail.com>
2025-12-30 16:57:10 -0600

    adding code

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,47 +1,43 @@
 import "./LoginModal.css";
 
 function LoginModal({ isOpen, onClose }) {
-    if (!isOpen) return null;
-  
-    return (
-      <div className="modal">
-        <div className="modal__overlay" onClick={onClose}></div>
-  
-        <div className="modal__content">
-          <button
-            type="button"
-            className="modal__close"
-            onClick={onClose}
-          ></button>
-  
-          <h2 className="modal__title">Sign in</h2>
-  
-          <label className="modal__label">
-            Email
-            <input
-              type="email"
-              className="modal__input"
-              placeholder="Enter email"
-            />
-          </label>
-  
-          <label className="modal__label">
-            Password
-            <input
-              type="password"
-              className="modal__input"
-              placeholder="Enter password"
-            />
-          </label>
-  
-          <button className="modal__submit">Sign in</button>
-  
-          <p className="modal__switch">
-            or <span>Sign up</span>
-          </p>
-        </div>
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal">
+      <div className="modal__overlay" onClick={onClose}></div>
+
+      <div className="modal__content">
+      <button type="button" className="modal__close" onClick={onClose} aria-label="Close modal">
+      </button>
+        <h2 className="modal__title">Sign in</h2>
+
+        <label className="modal__label">
+          Email
+          <input
+            type="email"
+            className="modal__input"
+            placeholder="Enter email"
+          />
+        </label>
+
+        <label className="modal__label">
+          Password
+          <input
+            type="password"
+            className="modal__input"
+            placeholder="Enter password"
+          />
+        </label>
+
+        <button className="modal__submit">Sign in</button>
+
+        <p className="modal__switch">
+          or <span>Sign up</span>
+        </p>
       </div>
-    );
-  }
-  
-  export default LoginModal;
+    </div>
+  );
+}
+
+export default LoginModal;
