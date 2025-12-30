712b39f3df523346bb8baf949200533c52b7c073
Meya Warrior <meyawarrior@gmail.com>
2025-12-30 11:56:49 -0600

    fixing css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,23 +8,16 @@ function Header() {
           <p className="header__logo">NewsExplorer</p>
 
           <nav className="header__nav">
-            <button className="header__link header__link_active">
-              Home
-            </button>
-            <button className="header__button">
-              Sign in
-            </button>
+            <button className="header__link header__link_active">Home</button>
+            <button className="header__button">Sign in</button>
           </nav>
         </div>
-
-        {/* HERO SECTION */}
         <div className="header__hero">
-          <h1 className="header__title">
-            What’s going on in the world?
-          </h1>
+          <h1 className="header__title">What&apos;s going on in the world?</h1>
 
           <p className="header__subtitle">
-            Find the latest news on any topic and save them in your personal account.
+            Find the latest news on any topic and save them in your personal
+            account.
           </p>
 
           <form className="search-form">
@@ -33,10 +26,7 @@ function Header() {
               type="text"
               placeholder="Enter topic"
             />
-            <button
-              className="search-form__button"
-              type="submit"
-            >
+            <button className="search-form__button" type="submit">
               Search
             </button>
           </form>
