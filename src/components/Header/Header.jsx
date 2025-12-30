18408c3983980072b273afb89f0cc96b7caf3cca
Meya Warrior <meyawarrior@gmail.com>
2025-12-30 11:19:42 -0600

    adding css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,17 +3,44 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <div className="header__content">
-        <p className="header__logo">NewsExplorer</p>
+      <div className="header__overlay">
+        <div className="header__content">
+          <p className="header__logo">NewsExplorer</p>
 
-        <nav className="header__nav">
-          <button className="header__link header__link_active">
-            Home
-          </button>
-          <button className="header__button">
-            Sign in
-          </button>
-        </nav>
+          <nav className="header__nav">
+            <button className="header__link header__link_active">
+              Home
+            </button>
+            <button className="header__button">
+              Sign in
+            </button>
+          </nav>
+        </div>
+
+        {/* HERO SECTION */}
+        <div className="header__hero">
+          <h1 className="header__title">
+            What’s going on in the world?
+          </h1>
+
+          <p className="header__subtitle">
+            Find the latest news on any topic and save them in your personal account.
+          </p>
+
+          <form className="search-form">
+            <input
+              className="search-form__input"
+              type="text"
+              placeholder="Enter topic"
+            />
+            <button
+              className="search-form__button"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
       </div>
     </header>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,28 +3,6 @@ import "./Main.css";
 function Main() {
   return (
     <main className="main">
-      <section className="hero">
-        <h1 className="hero__title">
-          What's going on in the world?
-        </h1>
-
-        <p className="hero__subtitle">
-          Find the latest news on any topic and save them in your personal account.
-        </p>
-
-        <form className="search-form">
-          <input
-            className="search-form__input"
-            placeholder="Enter topic"
-          />
-          <button
-            type="submit"
-            className="search-form__button"
-          >
-            Search
-          </button>
-        </form>
-      </section>
     </main>
   );
 }
