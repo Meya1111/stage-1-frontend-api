8f69b21768c4bf0e4d483614f5cbb9efe20b5ca8
Meya Warrior <meyawarrior@gmail.com>
2026-01-04 12:30:22 -0600

    adding things

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,37 @@ import "./Main.css";
 function Main() {
   return (
     <main className="main">
+      <section className="author">
+        <div className="author__container">
+          <div className="author__avatar">
+            <div className="author__smiley">
+              <span className="author__eye author__eye_left"></span>
+              <span className="author__eye author__eye_right"></span>
+              <span className="author__smile"></span>
+            </div>
+
+            <p className="author__avatar-text">
+              Placeholder image.
+              <br />
+              Put an image of yourself here.
+            </p>
+          </div>
+          <div className="author__content">
+            <h2 className="author__title">About the author</h2>
+
+            <p className="author__text">
+              This block describes the project author. Here you should indicate
+              your name, what you do, and which development technologies you
+              know.
+            </p>
+
+            <p className="author__text">
+              You can also talk about your experience with TripleTen, what you
+              learned there, and how you can help potential customers.
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
